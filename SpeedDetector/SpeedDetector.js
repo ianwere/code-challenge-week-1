01b42891c06ccce4101d94cf7b46d83f0e42c6a9
Ian <ian.were@student.moringaschool.com>
2023-09-25 09:05:45 +0300

    first commit

---
+++ SpeedDetector/SpeedDetector.js
@@ -0,0 +1,19 @@
+function calculateDemeritPoints(speed) {
+    const speedLimit = 70;
+    const kmPerDemeritPoint = 5;
+  
+    if (speed <= speedLimit) {
+      return 'Ok';
+    }
+  
+    const excessSpeed = speed - speedLimit;
+    const demeritPoints = Math.floor(excessSpeed / kmPerDemeritPoint);
+  
+    if (demeritPoints >= 12) {
+      return 'License suspended';
+    }
+  
+    return `Points: ${demeritPoints}`;
+  }
+  
+  module.exports = calculateDemeritPoints;
